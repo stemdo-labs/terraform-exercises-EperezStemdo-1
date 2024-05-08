a0c49ffa79db2007e9c9145c3343d28eef1154c0
eperez <eperez@stemdo.io>
2024-05-08 16:27:56 +0200

    Ejercicio 8 finalizado

---
+++ soluciones/ejercicio-8/backend.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+  backend "azurerm" {
+      resource_group_name  = "rg1eperez-lab01"
+      storage_account_name = "tfstateeperezq4jv3" 
+      container_name       = "tfstateeperez"
+      key                  = "terraform.tfstate"
+  }
+
+}
+
+provider "azurerm" {
+  features {}
+}
+
