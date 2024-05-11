e9597032ba977cbe465a21a32128d58d9098efc0
eperez <eperez@stemdo.io>
2024-05-11 17:32:11 +0200

    Limpieza del código comentado

--- soluciones/modulo-weekly-exercise/main.tf
+++ soluciones/modulo-weekly-exercise/main.tf
@@ -1,24 +1,3 @@
-# terraform {
-#   required_providers {
-#     azurerm = {
-#       source  = "hashicorp/azurerm"
-#       version = "~>3.0"
-#     }
-#   }
-#   backend "azurerm" {
-#       resource_group_name  = "rg1eperez-lab01"
-#       storage_account_name = "sta1eperez" 
-#       container_name       = "tfstateeperez"
-#       key                  = "terraform.tfstate"
-#   }
-
-# }
-
-# provider "azurerm" {
-#   features {}
-# }
-
-
 module "vnet" {
   source = "./modules/vnet"
   vnet_name = var.vnet_name
@@ -55,9 +34,6 @@ resource "azurerm_subnet_network_security_group_association" "assoc" {
   network_security_group_id = module.networkgroup.secgrup_id
 }
 
-
-
-
 resource "azurerm_network_interface" "nic" {
   for_each            = var.network_interfaces
   name                = each.key
@@ -70,8 +46,6 @@ resource "azurerm_network_interface" "nic" {
     private_ip_address_allocation = each.value.private_ip_address_allocation
   }
 }
-
-
 
 resource "azurerm_linux_virtual_machine" "vm" {
  
@@ -101,7 +75,6 @@ resource "azurerm_linux_virtual_machine" "vm" {
   }
 }
 
-
 resource "azurerm_lb" "lb" {
   name                = var.lb_name
   location            = var.location
@@ -119,63 +92,3 @@ resource "azurerm_lb_backend_address_pool" "backpool" {
   name                 = var.lb_backend_address_pool_name
 }
 
-# module "resourcegroup" {
-#   source = "./modules/resourcegroup"
-#   resource_group_name = var.resource_group_name
-#   location = var.location
-# }
-
-
-# resource "azurerm_subnet" "this" {
-#   for_each = var.subnets
-#   name                 = each.key
-#   resource_group_name  = var.resource_group_name
-#   virtual_network_name = azurerm_virtual_network.this.name
-#   address_prefixes     = each.value.subnet_address_prefixes
-# }
-
-
-
-# resource "azurerm_network_interface" "nic" {
-#   depends_on = [ azurerm_subnet.subnet ]
-#   #for_each = var.network_interface
-#   # name                = each.value.name
-#   # location            = each.value.location
-#   # resource_group_name = each.value.resource_group_name
-#   name = var.name_ip_configuration
-#   location = var.location
-#   resource_group_name = var.resource_group_name
-#   ip_configuration {
-#     name                          = "internal"
-#     subnet_id                     = azurerm_subnet.subnet.id
-#     private_ip_address_allocation = var.private_ip_address_allocation
-#   }
-# }
-
-# resource "azurerm_linux_virtual_machine" "vm" {
-#   name                =  var.vm_name
-#   resource_group_name = var.resource_group_name
-#   location            = var.location
-#   size                = var.size
-#   admin_username      = var.admin_username
-#   network_interface_ids = [
-#     azurerm_network_interface.nic.id,
-#   ]
-
-#   os_disk {
-#     caching              = var.caching
-#     storage_account_type = var.storage_account_type
-#   }
-
-#     admin_ssh_key {
-#     username   = var.admin_username
-#     public_key = file("~/.ssh/id_rsa.pub")
-#   }
-
-#   source_image_reference {
-#     publisher = "Canonical"
-#     offer     = "0001-com-ubuntu-server-jammy"
-#     sku       = "22_04-lts"
-#     version   = "latest"
-#   }
-# }
