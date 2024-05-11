60b62b0478aac7b05240823f0de85c912f62ebf0
eperez <eperez@stemdo.io>
2024-05-11 09:12:04 +0200

    quitar proveedores

--- soluciones/modulo-weekly-exercise/main.tf
+++ soluciones/modulo-weekly-exercise/main.tf
@@ -1,22 +1,22 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>3.0"
-    }
-  }
-  backend "azurerm" {
-      resource_group_name  = "rg1eperez-lab01"
-      storage_account_name = "sta1eperez" 
-      container_name       = "tfstateeperez"
-      key                  = "terraform.tfstate"
-  }
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = "~>3.0"
+#     }
+#   }
+#   backend "azurerm" {
+#       resource_group_name  = "rg1eperez-lab01"
+#       storage_account_name = "sta1eperez" 
+#       container_name       = "tfstateeperez"
+#       key                  = "terraform.tfstate"
+#   }
 
-}
+# }
 
-provider "azurerm" {
-  features {}
-}
+# provider "azurerm" {
+#   features {}
+# }
 
 
 
